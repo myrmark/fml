51b876617e0325b4612e41be4c565a1405a3d701
Filip Malmberg <91657147+myrmark@users.noreply.github.com>
2022-12-06 11:00:54 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
+username=$(logname)
 cp fml.png /usr/share/icons/fml.png
-cp fml.desktop /home/$USER/.local/share/applications/fml.desktop
-echo "//internal.icomera.com/dfs /mnt/fs cifs credentials=/home/$USER/.smbcredentials,user	0	0" | sudo tee -a /etc/fstab
-echo "username:svc-supply-chain" > /home/$USER/.smbcredentials
+cp fml.desktop /home/$username/.local/share/applications/fml.desktop
+echo "username=svc-supply-chain" > /home/$username/.smbcredentials
 read -sp "Enter password for svc-supply-chain: " pwd
-echo "password:$pwd" >> /home/$USER/.smbcredentials
-chmod 600 /home/$USER/.smbcredentials
+echo "password=$pwd" >> /home/$username/.smbcredentials
+chmod 600 /home/$username/.smbcredentials
 mkdir /mnt/fs
 chmod 777 /mnt/fs
-chmod $USER:$USER /mnt/fs
+echo "//internal.icomera.com/dfs /mnt/fs cifs credentials=/home/$username/.smbcredentials,user	0	0" | sudo tee -a /etc/fstab
