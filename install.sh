4761a0c49e43851caf68eae87bedfffd5099bfe2
Filip Malmberg <91657147+myrmark@users.noreply.github.com>
2022-12-06 12:28:51 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 username=$(logname)
 apt install python3-pip -y
+apt install cifs-utils -y
 pip install pymysql PyQt5
 export QT_QPA_PLATFORM=xcb
 cp fml.png /usr/share/icons/fml.png
 cp fml.desktop /home/$username/.local/share/applications/fml.desktop
 cp fml.pyw /usr/local/bin/fml.pyw
 cp g.ui /usr/local/bin/g.ui
-echo "username=svc-supply-chain" > /home/$username/.smbcredentials
+echo "username=svc-supply-prod" > /home/$username/.smbcredentials
 read -sp "Enter password for svc-supply-prod: " pwd
 echo "password=$pwd" >> /home/$username/.smbcredentials
 chmod 600 /home/$username/.smbcredentials
